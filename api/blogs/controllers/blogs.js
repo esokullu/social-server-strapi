40e041f59543cf1a3fbe81f1b0ffb2ce94ee1292
Barry S <bary.11.16@gmail.com>
2021-11-12 11:38:11 +0700

    create blog post endpoint

--- api/blogs/controllers/blogs.js
+++ api/blogs/controllers/blogs.js
@@ -5,4 +5,36 @@
  * to customize this controller
  */
 
-module.exports = {};
+const validator = require('validator');
+const { sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+  async startBlogPost(ctx) {
+    try {
+      const query = ctx.query;
+      if(validator.isLength(query.title, {min:1, max: 255}) && validator.isLength(query.content, {min:1})) {
+        const blog = await strapi.services.blogs.create({
+          title: query.title,
+          content: query.content
+        })
+        const sanitizedBlog = sanitizeEntity(blog, {
+          model: strapi.models["blogs"],
+        });
+        ctx.send({
+          success: true,
+          id: sanitizedBlog.id
+        });
+      } else {
+        ctx.send({
+          success: false,
+          reason: "Title (up to 255 chars) and Content are required."
+        });
+      }
+    } catch (error) {
+      ctx.send({
+        success: false,
+        reason: "Internal Error."
+      });
+    }
+  }
+};
