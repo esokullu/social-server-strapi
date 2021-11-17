f26dcc519cbc84f61075ba0c7c22deaeecd97357
Barry S <bary.11.16@gmail.com>
2021-11-17 14:41:00 +0700

    unfollow endpoint

--- api/follows/controllers/follows.js
+++ api/follows/controllers/follows.js
@@ -36,10 +36,7 @@ module.exports = {
         following: query.id,
         follower: ctx.req.user.id
       })
-      const sanitizedFollow = sanitizeEntity(follow, {
-        model: strapi.models["follows"]
-      })
-      if(sanitizedFollow) {
+      if(follow) {
         return ctx.send({
           success: false,
           reason: "already follow"
@@ -58,5 +55,51 @@ module.exports = {
         reason: "Internal Error."
       });
     }
-  }
+  },
+
+  async unfollow(ctx) {
+    try {
+      const query = ctx.request.query
+      if(Object.keys(query).length === 0) {
+        ctx.send({
+          success: false,
+          reason: "No field to set"
+        })
+      }
+      
+      const user = await strapi.query('user', 'users-permissions').findOne({
+        id: query.id
+      })
+      if(!user) {
+        return ctx.send({
+          success: false,
+          reason: "Invalid user ID"
+        })
+      }
+
+      const follow = await strapi.services.follows.findOne({
+        following: query.id,
+        follower: ctx.req.user.id
+      })
+      if(!follow) {
+        return ctx.send({
+          success: false,
+          reason: "No follow edge found"
+        })
+      }
+
+      await strapi.services.follows.delete({
+        following: query.id,
+        follower: ctx.req.user.id
+      })
+      return ctx.send({
+        success: true,
+      });
+    } catch (error) {
+      ctx.send({
+        success: false,
+        reason: "Internal Error."
+      });
+    }
+  } 
 };
