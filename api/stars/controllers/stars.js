12d4e6376bf064f271f2129e17f8094a90de78e2
Barry S <bary.11.16@gmail.com>
2021-11-25 14:50:41 +0700

    /star endpoint

--- api/stars/controllers/stars.js
+++ api/stars/controllers/stars.js
@@ -5,4 +5,53 @@
  * to customize this controller
  */
 
-module.exports = {};
+const validator = require('validator');
+
+module.exports = {
+  async star(ctx) {
+    const query = ctx.query;
+    if(query.url && query.id) {
+      ctx.send({
+        success: false,
+        reason: "Can't use parameter Url and ID together."
+      });
+    }
+    else if(query.url) {
+      if(!validator.isURL(query.url)){
+        ctx.send({
+          success: false,
+          reason: "Wrong Url format."
+        });
+      }
+
+      let star = await strapi.services.stars.findOne({
+        url: query.url,
+        user: ctx.req.user.id,
+      })
+      if(!star) {
+        star = await strapi.services.stars.create({
+          url: query.url,
+          user: ctx.req.user.id,
+        })
+        const stars = await strapi.services.stars.find({
+          url: query.url,
+        })
+        ctx.send({
+          success: true,
+          count: stars.length
+        });
+      } else {
+        ctx.send({
+          success: false,
+          reason: "Already starred."
+        });
+      }
+    }
+    else if(query.id) {
+      ctx.send({
+        success: false,
+        reason: "Url or ID is required."
+      });
+    }
+  }
+};
