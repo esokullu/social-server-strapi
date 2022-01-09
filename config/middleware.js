fec2aea53d1a78dca322211bbc51db3a4fc706b6
Emre Sokullu <>
2022-01-09 17:00:51 +0300

    config updated

--- config/middleware.js
+++ config/middleware.js
@@ -1,8 +1,8 @@
 module.exports = {
     settings: {
       cors: {
-        //enabled: true, 
-        //origin: ["http://localhost:8000"]
+        enabled: true, 
+        origin: ["http://localhost", "https://4671-78-183-96-227.ngrok.io/"]
       },
     },
   };
--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 80),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '4764a9c0c21fc0698b0b475318827265'),
