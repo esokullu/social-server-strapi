96ff4085e2fa564a1de57135f8632e41ec4ebbd9
Emre Sokullu <>
2022-02-13 01:11:00 +0300

    file upload

---
+++ config/plugins.js
@@ -0,0 +1,23 @@
+module.exports = ({ env }) => ({
+    upload: {
+      config: {
+        provider: 'aws-s3',
+        providerOptions: {
+          accessKeyId: env('AWS_ACCESS_KEY_ID'),
+          secretAccessKey: env('AWS_ACCESS_SECRET'),
+          region: env('AWS_REGION'),
+          params: {
+            Bucket: env('AWS_BUCKET'),
+          },
+        },
+        breakpoints: {
+          xlarge: 1920,
+          large: 1000,
+          medium: 750,
+          small: 500,
+          xsmall: 64
+        },
+      },
+    },
+  });
+   
