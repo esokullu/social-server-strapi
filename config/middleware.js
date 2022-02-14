a90179a2c314c3ba6faa5a56f8b4250a20aba1b6
Emre Sokullu <>
2022-02-14 17:56:03 +0300

    uploadFile bugfix

--- config/middleware.js
+++ config/middleware.js
@@ -6,7 +6,8 @@ module.exports = {
           "http://localhost:1337", 
           "http://localhost", 
           "https://4671-78-183-96-227.ngrok.io", 
-          "http://4671-78-183-96-227.ngrok.io"
+          "http://4671-78-183-96-227.ngrok.io",
+          "https://8bb8-88-227-68-215.ngrok.io"
         ]
       },
     },
